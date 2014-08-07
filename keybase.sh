7e859bee94aff31a47ebcfebf4e4002a383c678b
David Tiersch <d.tiersch@gmail.com>
2014-08-07 15:35:28 +0200

    Subcommand btc,bitcoin refs #1

--- keybase.sh
+++ keybase.sh
@@ -142,6 +142,14 @@ function _keybase() {
                 esac
                 return 0
                 ;;
+            btc|bitcoin)
+                case $cur in
+                    -*)
+                        COMPREPLY+=($(compgen -W '-f --force' -- ${cur}))
+                        ;;
+                esac
+                return 0
+                ;;
         esac
     else
         COMPREPLY+=($(compgen -W "${commands}" -- ${cur}))
